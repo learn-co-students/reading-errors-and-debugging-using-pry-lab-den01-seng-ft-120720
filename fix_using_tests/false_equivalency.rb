877b0794e8031a6ee284c66cd85d15ed91f6f46b
Louis Leffler <louisleffler@gmail.com>
2020-11-23 19:26:29 +0000

    Done.

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,13 +1,11 @@
-# don't forget to add: require 'pry'
-
 def get_user_input
   gets.chomp
 end
 
 def prompt_user
   puts "What would you like to do?"
-  puts "1.) Eat a hamburger."
-  puts "2.) Eat a ham."
+   1 = "Eat a Hamburger."
+   2 = "Eats a ham."
 end
 
 def selection(num)
@@ -20,5 +18,5 @@ end
 
 def runner
   prompt_user
-  selection(get_user_input)
+  selection(num)
 end
--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,7 +1,10 @@
-# don't forget to add: require 'pry'
+require 'pry'
+
+#{player1} = Rocky
+#{player2} = Bullwinkle 
 
 def start_game(player1, player2)
-  "Hello #{player1} & #{playar2}"
+  "Hello #{player1} & #{player2}"
 end
 
 def play_game(player2)
