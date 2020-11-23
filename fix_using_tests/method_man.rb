30add1c5a0afae2fc0ca2ecce7b1d7cd6a124a55
Louis Leffler <louisleffler@gmail.com>
2020-11-23 22:28:15 +0000

    Done.

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,4 +1,3 @@
-require 'pry'
 
 #{player1} = Rocky
 #{player2} = Bullwinkle 
@@ -7,6 +6,6 @@ def start_game(player1, player2)
   "Hello #{player1} & #{player2}"
 end
 
-def play_game(player2)
-  binding.pry
+def play_game(player1, player2)
+  "#{player1} is better than #{player2}"
 end
